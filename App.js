b276913ecf612159c790acfd702e510387245034
Stacey <stacey6105@gmail.com>
2023-03-11 22:46:38 +0000

    Calculator functionality mostly complete

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, NativeModules } from "react-native";
 
 export default function App() {
   const divide = "\u00F7";
@@ -8,178 +7,286 @@ export default function App() {
   const minus = "\u2212";
   const add = "\u002B";
   const equal = "\u003D";
-  const [operator, setOperator] = useState("");
-  const [sum, setSum] = useState("");
   const [num, setNum] = useState("0");
   const [values, setValues] = useState([]);
   const handleClick = (value) => {
-    if (num === "0") {
+    if (
+      values[values.length - 1] === "/" ||
+      values[values.length - 1] === "*" ||
+      values[values.length - 1] === "-" ||
+      values[values.length - 1] === "+"
+    ) {
       setNum(value);
     } else {
-      setNum(num + value);
+      if (num === "0") {
+        setNum(value);
+      } else {
+        setNum(num + value);
+      }
     }
   };
   return (
     <View style={styles.container}>
-      <View style={styles.calculatorInput}>
-        <Text
-          adjustsFontSizeToFit={true}
-          numberOfLines={1}
-          style={styles.calculatorInputText}
-        >
-          {num}
-        </Text>
-      </View>
-      <View style={styles.equationBtns}>
-        <View style={styles.nums}>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => setNum("0")}
+      <View style={styles.calculatorBody}>
+        <View style={styles.calculatorInput}>
+          <Text
+            adjustsFontSizeToFit={true}
+            numberOfLines={1}
+            style={styles.calculatorInputText}
           >
-            <Text style={styles.text}>c</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              if (num[0] !== "-") setNum(`-${num}`);
-              if (num[0] === "-") setNum(num.slice(1));
-            }}
-          >
-            <Text style={styles.text}>+/-</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              if (num.slice(0, 2) !== "0.") setNum(`0.${num}`);
-              if (num.slice(0, 2) === "0.") {
-                const decimal = num.slice(2);
-                setNum(`0.00${decimal}`);
-              }
-            }}
-          >
-            <Text style={styles.text}>%</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              setOperator("/");
-            }}
-          >
-            <Text style={styles.text}>{divide}</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("7");
-            }}
-          >
-            <Text style={styles.text}>7</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("8");
-            }}
-          >
-            <Text style={styles.text}>8</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("9");
-            }}
-          >
-            <Text style={styles.text}>9</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => setOperator("*")}
-          >
-            <Text style={styles.text}>{multiply}</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("4");
-            }}
-          >
-            <Text style={styles.text}>4</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("5");
-            }}
-          >
-            <Text style={styles.text}>5</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("6");
-            }}
-          >
-            <Text style={styles.text}>6</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => setOperator("-")}
-          >
-            <Text style={styles.text}>{minus}</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("1");
-            }}
-          >
-            <Text style={styles.text}>1</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("2");
-            }}
-          >
-            <Text style={styles.text}>2</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("3");
-            }}
-          >
-            <Text style={styles.text}>3</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => setOperator(add)}
-          >
-            <Text style={styles.text}>{add}</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              handleClick("0");
-            }}
-          >
-            <Text style={styles.text}>0</Text>
-          </Pressable>
-          <View style={[styles.button, styles.shadow]}></View>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => setNum(num + ".")}
-          >
-            <Text style={styles.text}>.</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.button, styles.shadow]}
-            onPress={() => {
-              console.log("equal");
-            }}
-          >
-            <Text style={styles.text}>{equal}</Text>
-          </Pressable>
+            {num}
+          </Text>
+        </View>
+        <View style={styles.equationBtns}>
+          <View style={styles.nums}>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => setNum("0")}
+            >
+              <Text style={styles.text}>c</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                if (num[0] !== "-") setNum(`-${num}`);
+                if (num[0] === "-") setNum(num.slice(1));
+              }}
+            >
+              <Text style={styles.text}>+/-</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                if (num.slice(0, 2) !== "0.") setNum(`0.${num}`);
+                if (num.slice(0, 2) === "0.") {
+                  const decimal = num.slice(2);
+                  setNum(`0.00${decimal}`);
+                }
+              }}
+            >
+              <Text style={styles.text}>%</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                setValues((curr) => {
+                  return [...curr, num, "/"];
+                });
+              }}
+            >
+              <Text style={styles.text}>{divide}</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("7");
+              }}
+            >
+              <Text style={styles.text}>7</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("8");
+              }}
+            >
+              <Text style={styles.text}>8</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("9");
+              }}
+            >
+              <Text style={styles.text}>9</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                setValues((curr) => {
+                  return [...curr, num, "*"];
+                });
+              }}
+            >
+              <Text style={styles.text}>{multiply}</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("4");
+              }}
+            >
+              <Text style={styles.text}>4</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("5");
+              }}
+            >
+              <Text style={styles.text}>5</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("6");
+              }}
+            >
+              <Text style={styles.text}>6</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                setValues((curr) => {
+                  return [...curr, num, "-"];
+                });
+              }}
+            >
+              <Text style={styles.text}>{minus}</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("1");
+              }}
+            >
+              <Text style={styles.text}>1</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("2");
+              }}
+            >
+              <Text style={styles.text}>2</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("3");
+              }}
+            >
+              <Text style={styles.text}>3</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                setValues((curr) => {
+                  return [...curr, num, "+"];
+                });
+              }}
+            >
+              <Text style={styles.text}>{add}</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                handleClick("0");
+              }}
+            >
+              <Text style={styles.text}>0</Text>
+            </Pressable>
+            <View
+              style={[
+                { backgroundColor: "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+            ></View>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => setNum(num + ".")}
+            >
+              <Text style={styles.text}>.</Text>
+            </Pressable>
+            <Pressable
+              style={({ pressed }) => [
+                { backgroundColor: pressed ? "white" : "#ffd3e8" },
+                styles.button,
+                styles.shadow,
+              ]}
+              onPress={() => {
+                const equation = values.join("");
+                const result = eval(equation + num);
+                setValues([]);
+                setNum(result);
+              }}
+            >
+              <Text style={styles.text}>{equal}</Text>
+            </Pressable>
+          </View>
         </View>
       </View>
     </View>
@@ -189,22 +296,34 @@ export default function App() {
 const styles = StyleSheet.create({
   button: {
     alignItems: "center",
-    backgroundColor: "#ffd3e8",
     borderRadius: "50%",
     padding: 5,
     display: "flex",
     justifyContent: "center",
-    height: 75,
-    width: 75,
+    height: 65,
+    width: 65,
     margin: 5,
+  },
+  calculatorBody: {
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingTop: 50,
+    paddingBottom: 25,
+    backgroundColor: "#e9e2ff",
+    borderRadius: 40,
+    width: "105%",
   },
   calculatorInput: {
     display: "flex",
     alignItems: "flex-end",
-    justifyContent: "flex-end",
-    width: "100%",
-    paddingLeft: 30,
-    paddingRight: 35,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    borderRadius: 20,
+    justifyContent: "center",
+    width: "93%",
+    minHeight: 70,
+    marginBottom: 10,
+    paddingLeft: 12,
+    paddingRight: 12,
   },
   calculatorInputText: {
     fontSize: 60,
@@ -215,6 +334,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     paddingTop: 150,
+    paddingHorizontal: 30,
   },
   nums: {
     alignItems: "center",
